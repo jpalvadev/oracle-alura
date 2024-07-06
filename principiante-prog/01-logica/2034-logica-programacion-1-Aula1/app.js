4121ca68644e66e18ab8d8b1c3bed8f641e49df5
Juan Pablo Alvarez <jpalvadev@gmail.com>
2024-07-06 20:00:05 -0300

    acomodando archivos

--- principiante-prog/01-logica/2034-logica-programacion-1-Aula1/app.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt('Ingrese su nombre');
-
-let edad = prompt('Ingrese su edad');
-
-if (edad >= 18) {
-    alert(`${nombre} es mayor de edad. Aca tenes el carnet de conducir`);
-} else {
-    alert(`${nombre} es menor de edad. no tenes carnet viejita`);
-}
--- principiante-prog/01-logica/2034-logica-programacion-1-Aula2/app.js
+++
@@ -1,7 +0,0 @@
-// let saldo = prompt('ingrese su saldo');
-
-// console.log(`Su saldo es: ${saldo}`);
-
-// let nombre = prompt('ingrese su nombre');
-
-// alert(`Bienvenido a mi fiestita ${nombre}!!`);
